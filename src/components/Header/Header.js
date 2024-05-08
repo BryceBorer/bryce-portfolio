47e131587c64445b5259499c20ee83ce19934bf3
Bryce Borer <136652932+CanadianWall@users.noreply.github.com>
2024-05-08 09:30:55 -0400

    starting work on the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,28 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="header__contact">
-        
+        <ul className="header__contact--list">
+            <li className="header__contact__contact">
+                <button className="header__contact__contact--button">
+                  Contact
+                </button>
+            </li>
+            <li className="header__contact__work">
+                <button className="header__contact__work--button">
+                  Work Experience
+                </button>
+            </li>
+            <li className="header__contact__projects">
+                <button className="header__contact__projects--button">
+                  Projects
+                </button>
+            </li>
+            <li className="header__contact__resume">
+                <button className="header__contact__resume--button">
+                  Resume
+                </button>
+            </li>
+        </ul>
       </nav>
 
     </header>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,28 +10,7 @@ const Hero = () => {
       <h1>Bryce Borer</h1>
       <h2>Software Engineer</h2>
       <h3>I thrive on turning complex problems into elegant software solutions.</h3>
-      <nav className="hero__contact">
-        <ul className="hero__contact--list">
-            <li className="hero__contact__github">
-                <a className="hero__contact__github--link"
-                href="https://github.com/CanadianWall">
-                    <img className="hero__contact__github--svg"
-                    src={githubIcon}
-                    alt="Github link"
-                    />
-                </a>
-            </li>
-            <li className="hero__contact__linkedin">
-                <a className="hero__contact__linkedin--link"
-                href="https://www.linkedin.com/in/bryceptborer/">
-                    <img className="hero__contact__linkedin--svg"
-                    src={linkedinIcon}
-                    alt="LinkedIn link"
-                    />
-                </a>
-            </li>
-        </ul>
-      </nav>
+      
 
     </section>
   );
