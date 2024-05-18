59475b2d77d37c4c19a54e0f089bbc47b79ba32d
Bryce Borer <136652932+CanadianWall@users.noreply.github.com>
2024-05-18 09:19:49 -0400

    adding some mixins and my headshot picture

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,25 @@
 import "./Hero.scss";
 import githubIcon from "../../assets/icons/github_icon.svg"
 import linkedinIcon from "../../assets/icons/linkedin_icon.svg"
+import bryceImage from "../../assets/images/bryce.jpg"
 
 const Hero = () => {
-  
+
 
   return (
     <section className="hero">
-      <h1>Bryce Borer</h1>
-      <h2>Software Engineer</h2>
-      <h3>I thrive on turning complex problems into elegant software solutions.</h3>
-      
-
+      <div className="hero__content">
+      <img className="hero__content__headshot" 
+        src={bryceImage}
+        alt="Bryce headshot"
+        />
+        <div className="hero__content__text">
+          <h1>Bryce Borer</h1>
+          <h2>Software Engineer</h2>
+          <h3>I thrive on turning complex problems into elegant software solutions.</h3>
+        </div>
+        
+      </div>
     </section>
   );
 };
