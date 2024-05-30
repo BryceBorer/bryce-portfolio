c762bbdee313b52a1a2c82e9696e5f3dda6ab04a
Bryce Borer <136652932+CanadianWall@users.noreply.github.com>
2024-05-30 08:03:23 -0400

    more changes to the Project cards

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -6,58 +6,58 @@ const Experience = () => {
   return (
     <section className="exp">
       <div className="exp--wrapper">
-      <div className="exp__card">
-        <h2 className="exp__card--title">
-          Skills
-        </h2>
-        <ul className="exp__card__list">
-          <li>
-            Lorem ipsum
-          </li>
-          <li>
-            Lorem ipsum
-          </li>
-          <li>
-            Lorem ipsum
-          </li>
-        </ul>
-      </div>
-      </div>
-      <div className="exp--wrapper">
-      <div className="exp__card">
-        <h2 className="exp__card--title">
-          Experience
-        </h2>
-        <ul className="exp__card__list">
-          <li>
-            Lorem ipsum
-          </li>
-          <li>
-            Lorem ipsum
-          </li>
-          <li>
-            Lorem ipsum
-          </li>
-        </ul>
-      </div>
+        <div className="exp__card">
+          <h2 className="exp__card--title">
+            Skills
+          </h2>
+          <ul className="exp__card__list">
+            <li>
+              Lorem ipsum
+            </li>
+            <li>
+              Lorem ipsum
+            </li>
+            <li>
+              Lorem ipsum
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="exp--wrapper">
-      <div className="exp__card">
-        <h2 className="exp__card--title">
-          Education
-        </h2>
-        <ul className="exp__card__list">
-          <li>
-            Lorem ipsum
-          </li>
-          <li>
-            Lorem ipsum
-          </li>
-          <li>
-            Lorem ipsum
-          </li>
-        </ul>
+        <div className="exp__card">
+          <h2 className="exp__card--title">
+            Experience
+          </h2>
+          <ul className="exp__card__list">
+            <li>
+              Lorem ipsum
+            </li>
+            <li>
+              Lorem ipsum
+            </li>
+            <li>
+              Lorem ipsum
+            </li>
+          </ul>
+        </div>
       </div>
+      <div className="exp--wrapper">
+        <div className="exp__card">
+          <h2 className="exp__card--title">
+            Education
+          </h2>
+          <ul className="exp__card__list">
+            <li>
+              Lorem ipsum
+            </li>
+            <li>
+              Lorem ipsum
+            </li>
+            <li>
+              Lorem ipsum
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
